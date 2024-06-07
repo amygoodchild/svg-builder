65a977526814d049338672ee66b4dd1599f82f37
Amy Goodchild <amy@amygoodchild.com>
2024-06-07 17:09:11 +0100

    Create svg.js

---
+++ svg.js
@@ -0,0 +1,72 @@
+class SvgBuilder{
+  constructor(){
+    this.groupid = 0;
+    this.svgEnded = false;
+    this.start();
+  }
+
+  // Starts the svg, defaults to my normalised canvas size (set elsewhere), or can be provided with a width and height
+  start(w = sz.nw, h = sz.nh){
+    this.s = "<svg width='" + w + "' height='" + h + "'>";
+    this.s += "\n";
+    this.s += "\n";
+  }
+
+  // Start a group, either with a provided id or defaults to incremented number 
+  startGroup(id = this.groupid){
+    this.s += "\n";
+    this.s += "<g id=" + id +  ">";
+    this.s += "\n";
+
+    this.groupid++;
+  }
+
+  // End a group
+  endGroup(){
+    this.s += "\n";
+    this.s += "</g>";
+    this.s += "\n";
+  }
+
+  // Add a comment to the svg 
+  addComment(comment){
+    this.s += "<!-- " + comment + " -->";
+    this.s += "\n";
+  }
+
+  // Add a path in the format [{x:0,y:0},{x:0,y:0}...]
+  addPath(path){
+    this.s += "<path d='";
+    
+    for(let p of path){
+      if (path.indexOf(p) == 0) this.s += "M ";
+      else this.s += "L ";
+      this.s += nf(p.x,0,2) + " " + nf(p.y,0,2) + " ";
+    }
+
+    this.s += "' fill='none' stroke='black' stroke-width='1'/>";
+    this.s += "\n";
+    this.s += "\n";
+  }
+
+  // Ends the svg
+  end(){
+    this.s  += "</svg>";
+    this.svgEnded = true;
+  }
+
+  // Downloads svg with given filename or default to image.svg
+  save(filename = "image.svg"){
+    // If we didn't call this from elsewhere in the code, end the svg
+    if (!this.svgEnded) this.end();
+
+    const blob = new Blob([this.s], { type: 'image/svg+xml' });
+    const url = URL.createObjectURL(blob);
+    const link = Object.assign(document.createElement('a'), {
+        href: url,
+        download: filename
+    });
+    link.click();
+    URL.revokeObjectURL(url);
+  }
+}
